8fe806cb98f5cd14f40907d3f8e0396bfe66ffbf
Raphael Bottino <raphabot@gmail.com>
2015-03-14 15:56:36 -0300

    Core updated to use the PartFileAbstract and CloudFileAbstract classes. Saving to database.

--- src/main/java/models/logic/Core.java
+++ src/main/java/models/logic/Core.java
@@ -1,5 +1,6 @@
 package models.logic;
 
+import DAO.Utils.SimpleEntityManager;
 import models.abstracts.ProviderAbstract;
 
 import java.io.BufferedReader;
@@ -8,14 +9,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import models.abstracts.FilePartAbstract;
+import models.file.CloudFile;
+import models.file.FilePart;
+import services.CloudFileService;
+import services.FilePartService;
 
 /**
  * Created by raphabot on 02/02/15.
  */
 public class Core {
 
-    public static ArrayList <String> encodeSplitUpload(String filePath, ArrayList<ProviderAbstract> providers) throws NoSuchAlgorithmException, IOException{
+    private static SimpleEntityManager simpleEntityManager;
+    
+    public static boolean encodeSplitUpload(String filePath, ArrayList<ProviderAbstract> providers) throws NoSuchAlgorithmException, IOException{
 
+        //Open file
+        File file = new File(filePath);
+        
         //Encode file
         
         
@@ -23,31 +34,39 @@ public class Core {
         String md5 = utils.MD5Generator.generate(filePath);
 
         //Save to db
-        
+        CloudFile cloudFile = new CloudFile(file.getName(), md5);
+        CloudFileService cfs = new CloudFileService(simpleEntityManager);
+        cfs.save(cloudFile);
 
         //Slipt file
-        ArrayList <String> remoteFilePaths = new ArrayList <String>();
         int numParts = providers.size();
-        
-        File file = new File(filePath);
         Splitter splitter = new Splitter(file);
         splitter.split(numParts);
 
-        //Upload files
+        //Iterate over each splitted file
         for (int i = 0; i < numParts; i++){
             ProviderAbstract provider = providers.get(i);
             try{
-                remoteFilePaths.add(provider.uploadFile(filePath + ".part." + i, "part" + i));
+                String partFilePath = filePath + ".part." + i;
+                
+                //Upload to provider
+                String remotePath = provider.uploadFile(partFilePath, "part" + i);
                 
                 //Calculate MD5
+                md5 = utils.MD5Generator.generate(partFilePath);
+                
+                //Save to db
+                FilePartAbstract filePart = new FilePart(providers.get(i), i, remotePath, md5);
+                FilePartService fps = new FilePartService(simpleEntityManager);
+                fps.save(filePart);
                 
                 
-                //Sabe to db
-                
-                
-            }catch (Exception e){return null;}
+            }
+            catch (Exception e){
+                return false;
+            }
         }
-        return remoteFilePaths;
+        return true;
     }
 
     public static boolean downloadMergeDecode(String filePath, ArrayList <String> remoteFilePaths, ArrayList<ProviderAbstract> providers){
